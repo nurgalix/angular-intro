c85b94fe900a34d32842b64973eff8c100c2c940
Nurgali Nuraly <nurali.nurgali@gmail.com>
2019-12-22 20:00:34 +0600

    Life cycle methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   name = 'Nurgali';
   today = new Date();
   counter = 0;
@@ -14,6 +14,9 @@ export class AppComponent {
     {name:'You', age:27}
   ];
 
+  ngOnInit() {
+    this.counter = 2;
+  }
   btnClicked = () => {
     this.counter ++;
   }
