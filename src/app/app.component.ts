6679bde503529062af14cfdee62364761adc7de7
Nurgali Nuraly <nurali.nurgali@gmail.com>
2019-12-22 12:16:34 +0600

    Using data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-intro';
+  name = 'Nurgali';
+  today = new Date();
 }
