a7cb50b83c0fb4889d7fca8033d924480d282060
Nurgali Nuraly <nurali.nurgali@gmail.com>
2019-12-26 22:20:01 +0600

    Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -18,6 +18,7 @@ import { SecondComponent } from './second/second.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [
